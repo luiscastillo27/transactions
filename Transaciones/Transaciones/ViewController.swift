0487084818b180845191441dd7ec34e3119f07f0
Luis Castillo <luisyosemite@gmail.com>
2019-02-21 01:02:09 -0600

    init proyect

---
+++ Transaciones/Transaciones/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Transaciones
+//
+//  Created by Luis on 2/21/19.
+//  Copyright © 2019 Luis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
